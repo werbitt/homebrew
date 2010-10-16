02bffc8ff0bb695a8166631986f62216ed5e23ae
Micah Werbitt <werbitt@gmail.com>
2010-10-16 11:32:28 -0400

    New Formula: dtrx
    dtrx is a tool for extracting archives and preventing tarbombs.

---
+++ Library/Formula/dtrx.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Dtrx < Formula
+	url 'http://brettcsmith.org/2007/dtrx/dtrx-7.0.tar.gz'
+	head 'http://www.brettcsmith.org/2007/dtrx/dtrx', :using => :hg
+	homepage 'http://brettcsmith.org/2007/dtrx/'
+	md5 '8297bd906088aedee840a32450efb1a2'
+
+	def install
+		system "python setup.py install --prefix=#{prefix}"
+	end
+end
